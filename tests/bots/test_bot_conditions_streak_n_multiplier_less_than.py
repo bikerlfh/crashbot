ee2783a11922f76e5c487093a5c891608e1b781a
lhenriquez <luis.henriquez@wearemo.com>
2023-10-15 01:57:08 -0500

    fixing tests

--- tests/bots/test_bot_conditions_streak_n_multiplier_less_than.py
+++ tests/bots/test_bot_conditions_streak_n_multiplier_less_than.py
@@ -100,3 +100,127 @@ class TestBotConditions2:
         assert bet_amount == 1000
         assert multiplier == 2
         assert ignore_model is False
+
+    def test_example_streak_multiplier_less_same(self):
+        """
+        Test streak 10 multiplier less than 10
+        in the configuration we have 2 condition with the same streak
+        :return:
+        """
+        conditions = [
+            BotCondition(
+                id=1,
+                condition_on=ConditionON.EVERY_WIN,
+                condition_on_value=1,
+                condition_on_value_2=None,
+                condition_action=ConditionAction.RESET_MULTIPLIER,
+                action_value=0,
+                others={},
+            ),
+            BotCondition(
+                id=2,
+                condition_on=ConditionON.EVERY_WIN,
+                condition_on_value=1,
+                condition_on_value_2=None,
+                condition_action=ConditionAction.RESET_BET_AMOUNT,
+                action_value=0,
+                others={},
+            ),
+            BotCondition(
+                id=3,
+                condition_on=ConditionON.STREAK_N_MULTIPLIER_LESS_THAN,
+                condition_on_value=10.0,
+                condition_on_value_2=10.0,
+                condition_action=ConditionAction.UPDATE_MULTIPLIER,
+                action_value=10.0,
+                others={},
+            ),
+            BotCondition(
+                id=4,
+                condition_on=ConditionON.STREAK_N_MULTIPLIER_LESS_THAN,
+                condition_on_value=10.0,
+                condition_on_value_2=5.0,
+                condition_action=ConditionAction.UPDATE_MULTIPLIER,
+                action_value=5.0,
+                others={},
+            ),
+        ]
+        helper = BotConditionHelper(
+            bot_conditions=conditions,
+            min_multiplier_to_bet=2,
+            min_multiplier_to_recover_losses=2,
+            multipliers=[1.0, 1.0, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.0, 2.6],
+        )
+        helper.initial_bet_amount = 1000
+        helper.current_bet_amount = 2000
+        helper.last_games = [False, False, False, False]
+        bet_amount, multiplier, ignore_model = helper.evaluate_conditions(
+            result_last_game=True,
+            multiplier_result=1.6,
+            profit=0.0,
+        )
+        assert bet_amount == 1000
+        assert multiplier == 10
+        assert ignore_model is False
+
+    def test_example_streak_multiplier_less_same_2(self):
+        """
+        Test streak 10 multiplier less than 10
+        in the configuration we have 2 condition with different same streak
+        :return:
+        """
+        conditions = [
+            BotCondition(
+                id=1,
+                condition_on=ConditionON.EVERY_WIN,
+                condition_on_value=1,
+                condition_on_value_2=None,
+                condition_action=ConditionAction.RESET_MULTIPLIER,
+                action_value=0,
+                others={},
+            ),
+            BotCondition(
+                id=2,
+                condition_on=ConditionON.EVERY_WIN,
+                condition_on_value=1,
+                condition_on_value_2=None,
+                condition_action=ConditionAction.RESET_BET_AMOUNT,
+                action_value=0,
+                others={},
+            ),
+            BotCondition(
+                id=3,
+                condition_on=ConditionON.STREAK_N_MULTIPLIER_LESS_THAN,
+                condition_on_value=10.0,
+                condition_on_value_2=10.0,
+                condition_action=ConditionAction.UPDATE_MULTIPLIER,
+                action_value=10.0,
+                others={},
+            ),
+            BotCondition(
+                id=4,
+                condition_on=ConditionON.STREAK_N_MULTIPLIER_LESS_THAN,
+                condition_on_value=5.0,
+                condition_on_value_2=5.0,
+                condition_action=ConditionAction.UPDATE_MULTIPLIER,
+                action_value=5.0,
+                others={},
+            ),
+        ]
+        helper = BotConditionHelper(
+            bot_conditions=conditions,
+            min_multiplier_to_bet=2,
+            min_multiplier_to_recover_losses=2,
+            multipliers=[1.0, 1.0, 1.5, 1.6, 1.7],
+        )
+        helper.initial_bet_amount = 1000
+        helper.current_bet_amount = 2000
+        helper.last_games = [False, False, False, False]
+        bet_amount, multiplier, ignore_model = helper.evaluate_conditions(
+            result_last_game=False,
+            multiplier_result=1.6,
+            profit=0.0,
+        )
+        assert bet_amount == 2000
+        assert multiplier == 5
+        assert ignore_model is False
